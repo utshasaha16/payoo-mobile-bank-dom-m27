dad19d5cb77d8b78918b02640001ffe84e307fbf
utshasaha16 <utsharoy12345@gmail.com>
2024-09-19 01:10:30 +0600

    added a home page

--- js/info.js
+++ js/info.js
@@ -2,6 +2,15 @@ document.getElementById('login-btn')
     .addEventListener('click', function(event){
         event.preventDefault();
 
-        const numberInput = document.getElementById('number-input').value ;
-        console.log(numberInput);
+        const numberInput = document.getElementById('number-input').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        // console.log(numberInput,pinNumber);
+
+        if(numberInput === '5' && pinNumber === '1234'){
+            console.log('You are logged in');
+            window.location.href = '/home.html'
+        }
+        else{
+            alert('wrong number and pin');
+        }
     })
