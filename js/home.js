470a94f3042247aec7919a3a5f9d7dcbe83e784a
utshasaha16 <utsharoy12345@gmail.com>
2024-09-19 01:32:51 +0600

    added parsially the money event handle

--- js/home.js
+++ js/home.js
@@ -1,1 +1,10 @@
-console.log('home added');
+// step-1
+document.getElementById('add-money-btn')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        // step-2 = get money added
+        const addMoneyInput = document.getElementById('add-money-input').value;
+        // step-3 = find the pin is right
+        const inputPinNumber = document.getElementById('input-pin-number').value;
+        console.log(addMoneyInput, inputPinNumber);
+    })
